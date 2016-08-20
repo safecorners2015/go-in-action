1f0056e0106c84a4cb6e280b72133b1acdaa693a
safecorners <safecorners2015@gmail.com>
2016-08-20 19:20:20 +0900

    concurrency #1

---
+++ chapter6/listing04/listing04.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	wg.Add(2)
+
+	fmt.Println("Execute go routine")
+	go printPrime("A")
+	go printPrime("B")
+
+	fmt.Println("Waiting...")
+	wg.Wait()
+
+	fmt.Println("finished program...")
+}
+
+func printPrime(prefix string) {
+	defer wg.Done()
+
+next:
+	for outer := 2; outer < 5000; outer++ {
+		for inner := 2; inner < outer; inner++ {
+			if outer%inner == 0 {
+				continue next
+			}
+		}
+		fmt.Printf("%s:%d\n", prefix, outer)
+	}
+	fmt.Println("Complete: ", prefix)
+}

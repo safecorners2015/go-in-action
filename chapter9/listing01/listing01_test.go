bfb5aa824857e8a1f2bd67bcc8cfb864cd7f6816
safecorners <safecorners2015@gmail.com>
2016-08-22 22:39:03 +0900

    basic unit test

---
+++ chapter9/listing01/listing01_test.go
@@ -0,0 +1,39 @@
+// a basic unit test.
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+const checkMark = "\2713"
+const ballotX = "\2717"
+
+func TestDownload(t *testing.T) {
+	url := "http://www.goinggo.net/feeds/posts/default?alt=rss"
+	statusCode := 200
+
+	t.Log("Given the need to test downloading content.")
+	{
+		t.Logf("\tWhen checking \"%s\" for status code \"%d\"",
+			url, statusCode)
+		{
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Fatal("\t\tShould be able to make the Get call.",
+					ballotX, err)
+			}
+			t.Log("\t\tShould be able to make the Get call.", checkMark)
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode == statusCode {
+				t.Logf("\t\tShould receive a \"%d\" status. %v",
+					statusCode, checkMark)
+			} else {
+				t.Errorf("\t\tShould receive a \"%d\" status. %v %v",
+					statusCode, ballotX, resp.StatusCode)
+			}
+		}
+	}
+}
